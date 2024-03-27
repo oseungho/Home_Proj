a3fe5cb64e7bd0f4cf00507d96a53d6c08f58c3e
oseungho <ostraderos@gmail.com>
2024-03-27 19:00:02 +0900

    페이지 기본 세팅부터 연습

--- myvue/src/components/HelloWorld.vue
+++ myvue/src/components/HelloWorld.vue
@@ -1,152 +1,59 @@
+<script setup>
+  import { ref } from 'vue'
+  import menuList from '@/pagejs/asideMenu'
+
+  const openedMenus = ref([])
+
+  const toggleSubMenu = (index) => {
+    const isOpened = openedMenus.value.includes(index)
+    if (isOpened) {
+      openedMenus.value = openedMenus.value.filter(item => item !== index)
+    } else {
+      openedMenus.value.push(index)
+    }
+  }
+</script>
+
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="logo"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+  <v-row no-gutters>
+    <v-col cols="2">
+      <v-container class="main-Area">
+        <aside>
+          <div>
+            sidebar            
+          </div>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to the Vuetify 3 Betassss
-        </h1>
-
-          <h4>Vite Preview</h4>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-5">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+          <ul v-if="menuList.length > 0">           
+            <li v-for="(menu, index) in menuList" :key="index" class="nav-link" @click="toggleSubMenu(index)">
+              <div style="border: solid 1px black;">
+                <a href="#">{{ menu.title }}</a>
+              </div>
+              <!-- 자식 메뉴가 있는 경우에만 렌더링 -->
+              <ul v-if="menu.children && openedMenus.includes(index)">
+                <li v-for="(child, childIndex) in menu.children" :key="childIndex" class="nav-link">
+                   <a href="#">{{ child.title }}</a>
+                </li>
+              </ul>
+            </li>
+          </ul>    
+        </aside>
+      </v-container>
+    </v-col>
+    <v-col cols="10">
+      <v-container class="main-Area">
+        main
+      </v-container>
+    </v-col>
+  </v-row>
 </template>
 
-<script>
-import logo from '../assets/logo.svg'
-
-export default {
-  name: 'HelloWorld',
-
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify/tree/next',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    logo,
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Roadmap',
-        href: 'https://vuetifyjs.com/introduction/roadmap/',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+<style>
+.main-Area{
+  border: solid 2px red;
 }
-</script>
+
+.nav-link a {
+    display: flex;
+    align-items: center;
+  }
+</style>
---
+++ myvue/src/pagejs/asideMenu.js
@@ -0,0 +1,37 @@
+export default [
+//   {
+//     title: '마이페이지',
+//     icon: { icon: 'mdi-face-man-shimmer-outline' },
+//     to: { name: 'apps-user-view-id', params: { id: 22 }  },
+//     meta: { counter: myRouteCounter++ },
+//   },
+
+//   {
+//     title: '커뮤니티',
+//     icon: { icon: 'mdi-account-group-outline' },
+//     children: [
+//       { title: '친구 / 구독자 관리 페이지', to: { name: 'community-user-tab', meta: { counter: friendRouteCounter++ }, params: { tab: 'friend' } }, icon: { icon: 'mdi-account-multiple-plus'  } },
+//       { title: '게시판', meta: { counter: postRouteCounter++ }, to: { name: 'community_post' }, icon: { icon: 'mdi-instagram'  } },
+//       { title: '채팅', meta: { counter: chatRouteCounter++ }, to: { name: 'apps-chat' }, icon: { icon: 'mdi-message-text'  } },
+//       { title: '메이트', meta: { counter: mateRouteCounter++ }, to: { name: 'mateList' }, icon: { icon: 'mdi-crowd'  } },
+//       { title: '챌린지', meta: { counter: challRouteCounter++ }, to: { name: 'challengeList' }, icon: { icon: 'mdi-charity'  } },
+//     ],
+//   },
+  
+//   {
+//     title: '자가진단',
+//     to: { name: 'selfTest' },
+//     icon: { icon: 'mdi-account-check-outline' },
+//     meta: { counter: selfRouteCounter++ },
+//   },  
+    {
+     title:'Python',
+     children:[
+        {title: 'Selenium'},
+        {title: 'BeautifulSoup'},
+     ]
+    },
+    {
+     title:'Java',   
+    }
+]
