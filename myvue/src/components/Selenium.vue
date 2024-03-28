f504acf1cf160d7807907153e1b573cf89dd6ad2
oseungho <ostraderos@gmail.com>
2024-03-28 17:05:42 +0900

    selenium 설명 작성중

--- myvue/src/components/Selenium.vue
+++ myvue/src/components/Selenium.vue
@@ -1,9 +1,28 @@
+<script setup>
+
+</script>
+
 <template>
     <div class="title">
         <h3>Selenium</h3>
+        <div style="border: solid 1px gray; border-radius: 15px;">
+            <span class="description">
+                - 웹 테스트 자동화 도구로 웹드라이버를 통해 브라우저를 제어해서 웹을 테스트하는 용도로 사용하는 라이브러리<br/>
+                - BeautifulSoup로 스크래핑이 안되는 동적 페이지나 로그인이 필요한 페이지 등을 스크래핑
+            </span>
+        </div>
     </div>
+    <br/>
     <div class="content">
-        <span>Selenium은 </span>
+        <p> 1. 라이브러리 설치 : pip install selenium</p>
+        <p> 2. 브라우저 제어 : 
+            <pre>
+                <code> 
+                    pip install selenium
+                    pip install selenium
+                </code>
+            </pre>
+        </p>        
     </div>
 </template>
 
@@ -12,7 +31,21 @@
     text-align: center;
 }
 
+.description {
+    display: block;
+    text-align: left;
+    margin: 10px; /* 원하는 여백 추가 */
+}
+
 .content{
-    padding: 5px;
+    padding: 20px;
+    border: 1px solid black;
+    border-radius: 15px;
+}
+
+pre{
+    /* width: 50%; */
+    background-color: rgb(220, 216, 211);
+    border-radius: 30px;
 }
 </style>
